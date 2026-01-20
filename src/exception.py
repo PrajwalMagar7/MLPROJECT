a285d997562526b6ab6ec1ba2197a67b909b2160
PrajwalMagar7 <prajwalmagar0000@gmail.com>
2026-01-20 15:03:41 +0545

    MLPROJECT

--- src/exception.py
+++ src/exception.py
@@ -3,12 +3,7 @@ import sys
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()                       ##exc_info()=tuple unpacking -> ignore previous 2 values and take last vlaue
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-    file_name,
-    exc_tb.tb_lineno,
-    str(error)
-)
-
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno.str(error))
     return error_message
 
 class CustomException(Exception):
